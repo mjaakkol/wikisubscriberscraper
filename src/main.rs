bfb5f59104948b22059c34707de881a270b01151
Mikko Jaakkola <mikko.la.jaakkola@gmail.com>
2021-06-14 17:49:34 -0700

    backup

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,69 @@
+use std::{
+    str::FromStr,
+    collections::HashMap,
+    fs::File,
+    io::Write,
+    path::Path,
+    borrow::Cow
+};
+
+use reqwest::{
+    Client,
+    //Error
+};
+
+//use lazy_static::lazy_static;
+use regex::Regex;
+
+//use serde::Serialize;
+//use serde::{Deserialize, Serialize};
+
+use serde_json::value::Value;
+
 use clap::{
     Arg,
     App
 };
 
-fn scrape() {
-    let europe = "https://en.wikipedia.org/w/rest.php/v1/page/List_of_mobile_network_operators_of_Europe";
+use scraper::{
+    Html,
+    Selector
+};
 
-    let echo_json: serde_json::Value = reqwest::Client::new()
-        .post("https://jsonplaceholder.typicode.com/posts")
-        .json(&serde_json::json!({
-            "title": "Reqwest.rs",
-            "body": "https://docs.rs/reqwest",
-            "userId": 1
-        }))
+use tokio::fs::read_to_string;
+
+async fn fetch(output_file: &Path) -> String {
+    let europe = "https://en.wikipedia.org/w/rest.php/v1/page/List_of_mobile_network_operators_of_Europe";
+    let europe = "https://en.wikipedia.org/w/api.php?action=parse&page=List_of_mobile_network_operators_of_Europe&prop=text&formatversion=2&format=json&disabletoc=true";
+
+    let client = Client::new();
+
+    let response = client
+        .get(europe)
         .send()
-        .await?
-        .json()
-        .await?;
+        .await.unwrap()
+        .json::<HashMap<String, Value>>()
+
+        .await.unwrap();
+
+    //println!("{:?}", response);
+
+    let j = serde_json::to_string(&response["source"]).expect("Converting JSON into string failed");
+
+    //let mut file = File::create(&output_file).expect("Opening file failed");
+    //file.write_all(&j.as_bytes()).expect("Writing JSON file failed");
+
+    j
+}
+
+fn parse_page(text: &str) {
+    let document = Document::from(text);
 
 }
 
-fn main() {
+#[tokio::main]
+async fn main()  {
+//async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = App::new("Wiki mobile subscriber scraper")
                             .version("0.1")
                             .author("mikko.la.jaakkola@gmail.com")
@@ -36,18 +79,28 @@ fn main() {
                                 .long("file")
                                 .help("Output format for the file")
                                 .value_name("OUTPUT_FORMAT")
+                                .required(true)
                                 .help("Output file format"))
                             .get_matches();
-                            /*.arg(Arg::with_name("v")
-                                .short("v")
-                                .multiple(true)
-                                .help("Sets the level of verbosity"))*/
 
     // Gets a value for config if supplied by user, or defaults to "default.conf"
     let output = matches.value_of("output").unwrap_or("default.json");
     println!("Value for output file: {}", output);
 
+    let output_file_path = Path::new(output);
+
     // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
     // required we could have used an 'if let' to conditionally get the value)
     println!("Using input file: {}", matches.value_of("format").unwrap());
+
+    let content = if output_file_path.exists() {
+        read_to_string(&output_file_path).await.unwrap()
+        //let deserialized: Value = serde_json::from_str(&serialized).unwrap();
+    }
+    else {
+        fetch(&output_file_path).await
+    };
+
+    parse_page(content);
+
 }
